6c0b3ccde26db42a7a349c9e87895270896ed790
Brokexprotocol <contact@brokex.trade>
2026-05-31 00:54:58 +0200

    feat: update lens trade struct and dynamic liqPrice calculation

--- backend/services/dbService.js
+++ backend/services/dbService.js
@@ -60,13 +60,32 @@ function formatTrade(t) {
     orderType: t.orderType !== undefined ? Number(t.orderType.toString()) : 0,
     margin: t.margin ? t.margin.toString() : "0",
     leverage: t.leverage ? Number(t.leverage.toString()) : 0,
-    openInterest: t.openInterest ? t.openInterest.toString() : "0",
+    openInterest: (() => {
+      if (t.openInterest) return t.openInterest.toString();
+      const marginVal = t.margin ? BigInt(t.margin.toString()) : 0n;
+      const leverageVal = t.leverage ? BigInt(t.leverage.toString()) : 0n;
+      return (marginVal * leverageVal).toString();
+    })(),
     targetPrice: t.targetPrice ? t.targetPrice.toString() : "0",
     openPrice: t.openPrice ? t.openPrice.toString() : "0",
     closePrice: t.closePrice ? t.closePrice.toString() : "0",
     stopLoss: t.stopLoss ? t.stopLoss.toString() : "0",
     takeProfit: t.takeProfit ? t.takeProfit.toString() : "0",
-    liqPrice: t.liqPrice ? t.liqPrice.toString() : "0",
+    liqPrice: (() => {
+      if (t.liqPrice) return t.liqPrice.toString();
+      const leverageVal = t.leverage ? BigInt(t.leverage.toString()) : 0n;
+      if (leverageVal === 0n) return "0";
+      const openPriceVal = t.openPrice && BigInt(t.openPrice.toString()) > 0n
+        ? BigInt(t.openPrice.toString())
+        : (t.targetPrice ? BigInt(t.targetPrice.toString()) : 0n);
+      const direction = t.direction !== undefined ? Number(t.direction.toString()) : 0;
+      const move = (openPriceVal * 900000n) / (leverageVal * 1000000n);
+      if (direction === 1) {
+        return (openPriceVal > move ? openPriceVal - move : 0n).toString();
+      } else {
+        return (openPriceVal + move).toString();
+      }
+    })(),
     maxProfit: t.maxProfit ? t.maxProfit.toString() : "0",
     openTimestamp: t.openTimestamp ? Number(t.openTimestamp.toString()) : 0,
     closeTimestamp: t.closeTimestamp ? Number(t.closeTimestamp.toString()) : 0
